65089781c51acc8bdbf8bdaa0c8808713a0e25f2
Mohammad Miras Ahmadi <94456884+Mohmmad-Miras@users.noreply.github.com>
2022-11-19 22:08:11 +0330

    Update Header.js

--- Components/Share/Header.js
+++ Components/Share/Header.js
@@ -14,15 +14,14 @@ const Header = () => {
         <div className="flex justify-between h-12 text-white items-center px-5">
             <div onClick={() => setIsShow(!isShow)} >
                 <MenuIcon  />
-            </div>
-          
+            </div>          
                 <ImageBase
                  src="/images/logo.png"
                  layout='fill'
                  className='absolute object-contain'
                  container="relative h-12 w-12 z-10"
-                 alt="test" />        
-            
+                 alt="test" 
+                />            
             <div>
                 <SearchIcon className='hover:text-Template-color3 ml-2' />
                 <PersonIcon className='hover:text-Template-color3 ' />
